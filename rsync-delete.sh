5d8a1a564fa9d743f85343cba109107accc755f1
Janpstrunn <156332097+janpstrunn@users.noreply.github.com>
2024-01-11 16:56:19 +0000

    Update rsync-delete.sh

--- rsync-delete.sh
+++ rsync-delete.sh
@@ -8,20 +8,6 @@ rsync -av --delete "$dir1" "$dir2"
 # If the command worked, then a message will appear
 if [ $? -eq 0 ]; then
   echo "Complete!"
-# An extra command may be run if desired, which looks for differences in both directories
-# making sure they are identical, if not, showing you were are the differences
-  read -p "Do you want to make sure both dirs are indeed identical? (y/n): " choice
-  if [ "$choice" = "y" ]; then
-    echo "Running diff"
-    diff -r "$dir1" "$dir2"
-# If the command worked, then a message will appear
-        if [ $? -eq 0 ]; then
-        echo "Complete!"
-      fi
-# This appear when the diff command isn't chosen
-  else
-    echo "Skipped"
-  fi
 # This appear only when the rsync command fails
 else
   echo "The command failed!"
