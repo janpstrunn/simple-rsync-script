091483141ad78078b00ac94a4e3f25ee387e2647
Janpstrunn <156332097+janpstrunn@users.noreply.github.com>
2024-01-13 16:19:17 +0000

    Update rsync-delete.sh

--- rsync-delete.sh
+++ rsync-delete.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
-# Variables for the source and the desired directory
 dir1="$1"
 dir2="$2"
-# Runs rsync, deleting everything else that isn't in the source, making a identical copy
 echo "Executing rsync delete"
 rsync -av --delete "$dir1" "$dir2"
-# If the command worked, then a message will appear
 if [ $? -eq 0 ]; then
   echo "Complete!"
-# This appear only when the rsync command fails
 else
   echo "The command failed!"
 fi
