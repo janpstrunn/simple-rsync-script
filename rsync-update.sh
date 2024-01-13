9549d63d70fcf8d62df8a23c3100699a4f0dac30
Janpstrunn <156332097+janpstrunn@users.noreply.github.com>
2024-01-13 16:19:35 +0000

    Update rsync-update.sh

--- rsync-update.sh
+++ rsync-update.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
-# Variables for the source and the desired directory
 dir1="$1"
 dir2="$2"
-# Runs rsync, updating everything else that from the source, and keeping the files that aren't in the source
 echo "Executing rsync delete"
 rsync -av --update "$dir1" "$dir2"
-# If the command worked, then a message will appear
 if [ $? -eq 0 ]; then
   echo "Complete!"
-# This appear only when the rsync command fails
 else
   echo "The command failed!"
 fi
